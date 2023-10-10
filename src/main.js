d2e56170d54a19857cde14f7325b4592a4536b04
llziraell <alex.sokolovva@gmail.com>
2023-10-11 00:19:01 +0300

    add Views and VueRouter paths

--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+import App from './App.vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+
+import { BootstrapVueNext } from 'bootstrap-vue-next'
+
+
 import { createPinia } from 'pinia'
 
-import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(BootstrapVueNext)
 
 app.mount('#app')
