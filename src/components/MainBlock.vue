8a8d530cc7096a00525647db12f7741ec49cce23
llziraell <alex.sokolovva@gmail.com>
2023-10-11 00:22:06 +0300

    add MainBlock

---
+++ src/components/MainBlock.vue
@@ -0,0 +1,26 @@
+<script setup>
+</script>
+
+<template>
+    <div >
+        <header>
+            <slot name="header">
+            </slot>
+        </header>
+        <div class = "container">
+            <slot name="container"></slot>
+        </div>
+        <footer>
+            <slot name="footer">
+            </slot>
+        </footer>
+    </div>
+</template>
+
+<style >
+.container{
+    background-color: #1a191f;
+    max-width: 100vw;
+    margin: 0 auto;
+}
+</style>
