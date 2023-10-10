7346acff5058ae4845685cb9e283c1fd7c16c3b9
llziraell <alex.sokolovva@gmail.com>
2023-10-11 00:24:27 +0300

    add NavBar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,26 @@
+<script setup></script>
+
+<template>
+    <b-navbar
+        toggleable="lg"
+        sticky="top"
+        class="bg-dark"
+    >
+        <b-navbar-nav>
+            <b-navbar-brand
+                href="#"
+                class="text-white"
+                >MovieApp</b-navbar-brand
+            >
+            <b-nav-item @click.prevent="$router.push(`${'bookmark'}`)">
+                Избранное
+            </b-nav-item>
+        </b-navbar-nav>
+        <b-navbar-nav>
+            <b-dropdown-item href="#">год</b-dropdown-item>
+            <b-dropdown-item href="#">рейтинг</b-dropdown-item>
+            <b-dropdown-item href="#">хронометраж</b-dropdown-item>
+            <search-block></search-block>
+        </b-navbar-nav>
+    </b-navbar>
+</template>
