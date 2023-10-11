adeff289a9b49a431317482879d74ab7b66dc757
llziraell <alex.sokolovva@gmail.com>
2023-10-11 18:33:10 +0300

    add demp BookmarksView

--- src/views/BookmarkView.vue
+++ src/views/BookmarkView.vue
@@ -1,10 +1,44 @@
 <script setup>
-
+import MainBlock from "@/components/MainBlock.vue"
+import NavBar from "@/components/NavBar.vue"
+import MovieCard from "@/components/MovieCard.vue"
 </script>
 
 <template>
-    <div>
-    </div>    
+    <main-block>
+        <template #header>
+            <nav-bar></nav-bar>
+        </template>
+        <template #container>
+            <div>
+                <b-carousel
+                    v-model="slide"
+                    :interval="4000"
+                    controls
+                    indicators
+                    background="#ababab"
+                   style = "width: 100%; height: 85vh;"
+                    @sliding-start="onSlideStart"
+                    @sliding-end="onSlideEnd"
+                >
+                    <b-carousel-slide
+                        caption="Избранное"
+                        style="color: #fff"
+                        text="..."
+                        
+                    >
+
+                </b-carousel-slide>
+                    <b-carousel-slide
+                        caption="Вы оценили"
+                        text="..."
+                    ></b-carousel-slide>
+                </b-carousel>
+
+            </div>
+        </template>
+        <template #footer> </template>
+    </main-block>
 </template>
 
 <style scoped>
